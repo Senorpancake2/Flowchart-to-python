f3c59829cb70a9f95996e452e837dac1aa3d5e0d
Senorpancake2 <Jadenhasenfus557@gmail.com>
2026-03-23 16:27:11 +0000

    finished

---
+++ code.py
@@ -0,0 +1,15 @@
+
+
+def only_ints(a,b):
+    print(type(a))
+    print(type(b))
+
+
+    if int == type(a) and int == type(b):
+        print("True")
+    else:
+        print("False")
+
+
+
+only_ints(1,2)
